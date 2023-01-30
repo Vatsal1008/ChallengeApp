269195a26223e00e3b2ebb30034da5cce78cbab4
Vatsal1008 <Vatsal1008@DESKTOP-16D1GQO>
2023-01-30 18:30:23 +0100

    Zadanie dzien 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var name = "Ewa";
+        var sex = "k";
+        int age = 33;
+
+        if (age < 30 && sex == "k")
+        {
+            Console.WriteLine("Kobieta poniżej 30 lat");
+        }
+        else if (age == 33 && name == "Ewa")
+        {
+            Console.WriteLine("Ewa, lat 33");
+        }
+        else if (age <= 18 && sex != "k")
+        {
+            Console.WriteLine("Niepełnoletni Mężczyzna");
+        }
+        else 
+        {
+            Console.WriteLine("Wprowadź prawidłowe dane");
+        }
+    }
+}
