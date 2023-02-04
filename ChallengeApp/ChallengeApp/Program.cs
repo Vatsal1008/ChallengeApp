16af2cc174b459eef32b7a4e37186fe9c86e1f3d
Vatsal1008 <Vatsal1008@DESKTOP-16D1GQO>
2023-02-04 16:57:41 +0100

    sobota 04-02-2023 user

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,55 +1,10 @@
-﻿long number = 5488265555;
-string numberInString = number.ToString();
-char[] letters = numberInString.ToCharArray();
+﻿using ChallengeApp;
 
-int number0 = 0;
-int number1 = 0;
-int number2 = 0;
-int number3 = 0;
-int number4 = 0;
-int number5 = 0;
-int number6 = 0;
-int number7 = 0;
-int number8 = 0;
-int number9 = 0;
+User user1 = new User("Artur", "32455");
+User user2 = new User("Nati", "009845");
+User user3 = new User("Marta", "676845");
+User user4 = new User("Gosia", "673576");
 
-foreach (char c in letters)
-{
-    if (c == '0')
-    { number0++; }
-    else if (c == '1')
-    { number1++; }
-    else if (c == '2')
-    { number2++; }
-    else if (c == '3')
-    { number3++; }
-    else if (c == '4')
-    { number4++; }
-    else if (c == '5')
-    { number5++; }
-    else if (c == '6')
-    { number6++; }
-    else if (c == '7')
-    { number7++; }
-    else if (c == '8')
-    { number8++; }
-    else if (c == '9')
-    { number9++; }
-}
-Console.WriteLine(number);
-Console.WriteLine("0 => "+ number0);
-Console.WriteLine("1 => "+ number1);
-Console.WriteLine("2 => " +number2);
-Console.WriteLine("3 => " +number3);
-Console.WriteLine("4 => " +number4);
-Console.WriteLine("5 => " +number5);
-Console.WriteLine("6 => " +number6);
-Console.WriteLine("7 => " +number7);
-Console.WriteLine("8 => " +number8);
-Console.WriteLine("9 => " +number9);
-
-
-
-
-
-    
+user1.AddScore(5);
+user1.AddScore(2);
+var result = user1.Result;
---
+++ ChallengeApp/ChallengeApp/User.cs
@@ -0,0 +1,30 @@
+﻿namespace ChallengeApp
+{
+    public class User
+    {
+        private List<int> score = new List<int>();
+        private string login;
+        private string password;
+        public User(string login, string password)
+        {
+            this.login = login;
+            this.password = password;
+
+        }
+        public string Login { get; private set; }
+        public string Password { get; private set; }
+
+        public int Result
+        {
+            get
+            {
+                return this.score.Sum();
+            }
+        }
+
+        public void AddScore(int number)
+        {
+            this.score.Add(number);
+        }
+    }
+}
